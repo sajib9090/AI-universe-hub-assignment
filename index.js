6bb1bc060e49906e8249d745a1ffc89c1fe98a51
sajib9090 <nispap.devil.sajib@gmail.com>
2023-03-03 01:35:12 +0600

    add primary modal

--- index.js
+++ index.js
@@ -9,27 +9,28 @@ const showFeatures = () => {
 const displayFeatures = (features) => {
     // console.log(Features)
     const featuresContainer = document.getElementById('display-features-container');
-
-    features.forEach((singleFeature) => {
-        console.log(singleFeature)
+    features.slice(0, 6).forEach((singleFeature) => {
+        // console.log(singleFeature)
+        const {image, features, name, published_in} = singleFeature;
         const createDiv = document.createElement('div');
         createDiv.classList.add('col');
         createDiv.innerHTML = `
         <div class="card h-100">
-        <img src="${singleFeature.image}" class="card-img-top p-3" alt="...">
+        <img src="${image}" class="card-img-top p-3" alt="...">
         <div class="card-body">
           <h5 class="card-title fs-3 fw-semibold">Features</h5>
-          <p class="fs-6">1. ${singleFeature.features[0]}</p>
-          <p class="fs-6">2. ${singleFeature.features[1]}</p>
-          <p class="fs-6">3. ${singleFeature.features[2]}</p>
+          <p class="fs-6">1. ${features[0]}</p>
+          <p class="fs-6">2. ${features[1]}</p>
+          <p class="fs-6">3. ${features[2]}</p>
         </div>
-        <div class="card-footer bg-transparent">
+        <div class="px-3">
+        <hr class="text-secondary">
            <div>
-               <h5 class="card-title fs-3 fw-semibold py-3">${singleFeature.name}</h5>
+               <h5 class="card-title fs-3 fw-semibold py-3">${name}</h5>
            </div>
            <div class="d-flex align-items-center justify-content-between">
-               <p><i class="fa fa-calendar-days"></i> ${singleFeature.published_in}</p>
-               <p><i class="fa fa-arrow-right bg-body-tertiary p-2 rounded-circle text-danger"></i></p>
+               <p><i class="fa fa-calendar-days"></i> ${published_in}</p>
+               <p><i style="cursor: pointer;" class="fa fa-arrow-right bg-body-tertiary p-2 rounded-circle text-danger" data-bs-toggle="modal" data-bs-target="#exampleModal"></i></p>
            </div>
         </div>
       </div>
@@ -37,4 +38,8 @@ const displayFeatures = (features) => {
         featuresContainer.appendChild(createDiv)
     })
 }
-showFeatures()
+
+
+const detailsInfo = () => {
+    const modalDetails = document.getElementById()
+}
